1691b2b0308640c8650db82857b94b54b92911f7
oppenheimer3 <meskinehocine2@gmail.com>
2024-01-19 22:45:17 +0100

    var

--- vae/main.py
+++ vae/main.py
@@ -16,17 +16,18 @@ class VAE(nn.Module):
     self.sig = nn.Sigmoid()
     self.softplus = nn.Softplus()
     self.pz = MultivariateNormal(torch.zeros(100),torch.eye(100))
+    self.q = self.pz
 
 
   def forward(self, x):
     mean, covariance = self.encoder(x)
-    q = MultivariateNormal(mean, torch.eye(100) * covariance)
+    self.q = MultivariateNormal(mean, torch.eye(100) * covariance)
     z = mean + covariance * self.pz.sample([10,])
     pv_z =  self.pv_z(x, z)
     E_px_v = torch.mean(torch.log(pv_z))
-    Dkl = torch.mean(self.pz.log_prob(z) - q.log_prob(z))
+    Dkl = torch.mean(self.pz.log_prob(z) - self.q.log_prob(z))
     l = E_px_v + Dkl
-    return l
+    return -l
 
   def encoder(self, x):
     mean = torch.mean(self.softplus(self.l0(x)), axis=0)
@@ -34,12 +35,21 @@ class VAE(nn.Module):
     return mean, covariance
 
   def decoder(self, z):
-    return self.sig(self.l2(z))
+    return self.l2(z)
 
   def pv_z(self, x, z):
-    p = self.decoder(z)
+    y = self.decoder(z)
     x = x.unsqueeze(1)
-    return torch.prod(self.sig((2*x - 1) * p), axis=-1)
+    return torch.prod(self.sig((2*x - 1) * y), axis=-1) + 0.001
+  
+  def sample(self):
+    z = self.q.sample([100])
+    p = self.sig(self.decoder(z))
+    return torch.bernoulli(p)
+  
+
+
+
 
 
  
