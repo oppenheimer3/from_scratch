b9af333c2509a8b6e42ebd16afbdf328f1aef022
oppenheimer3 <meskinehocine2@gmail.com>
2024-01-20 21:18:27 +0100

    hh

--- vae/main.py
+++ vae/main.py
@@ -10,42 +10,52 @@ import argparse
 class VAE(nn.Module):
   def __init__(self):
     super().__init__()
-    self.l0 = nn.Linear(784,100)
-    self.l1 = nn.Linear(784,100)
-    self.l2 = nn.Linear(100, 784)
+    self.l = nn.Linear(784, 500)
+    self.l0 = nn.Linear(500,100)
+    self.l1 = nn.Linear(500,100)
+    self.l2 = nn.Linear(100, 1000)
+    self.l3 = nn.Linear(1000, 784)
     self.sig = nn.Sigmoid()
     self.softplus = nn.Softplus()
-    self.pz = MultivariateNormal(torch.zeros(100),torch.eye(100))
-    self.q = self.pz
+
 
 
   def forward(self, x):
-    mean, covariance = self.encoder(x)
-    self.q = MultivariateNormal(mean, torch.eye(100) * covariance)
-    z = mean + covariance * self.pz.sample([10,])
-    pv_z =  self.pv_z(x, z)
-    E_px_v = torch.mean(torch.log(pv_z))
-    Dkl = torch.mean(self.pz.log_prob(z) - self.q.log_prob(z))
-    l = E_px_v + Dkl
-    return -l
+    mu, log_var = self.encoder(x)
+    std = torch.exp(0.5 * log_var)
+    eps = torch.randn_like(std)
+    z = mu + eps * std 
+    return self.decoder(z), mu, std
+
 
   def encoder(self, x):
-    mean = torch.mean(self.softplus(self.l0(x)), axis=0)
-    covariance = torch.mean(self.softplus(self.l1(x)), axis=0)
-    return mean, covariance
+    x = self.softplus(self.l(x))
+    return self.softplus(self.l0(x)), self.softplus(self.l1(x))
 
   def decoder(self, z):
-    return self.l2(z)
+    z = self.softplus(self.l2(z))
+    return self.l3(z)
+
 
   def pv_z(self, x, z):
     y = self.decoder(z)
     x = x.unsqueeze(1)
-    return torch.prod(self.sig((2*x - 1) * y), axis=-1) + 0.001
-  
-  def sample(self):
-    z = self.q.sample([100])
-    p = self.sig(self.decoder(z))
-    return torch.bernoulli(p)
+    return torch.prod(self.sig((2*x - 1) * y), axis=-1).clamp(min=1e-9)
+
+def loss(x, y, mu, std):
+    q = MultivariateNormal(loc=mu, covariance_matrix=torch.diag(std**2))
+    pz = MultivariateNormal(loc=torch.zeros_like(mu), covariance_matrix=torch.eye(100))
+    z = q.sample([10,])
+    x = x.unsqueeze(1)
+    pv_z = torch.prod(F.sigmoid((2*x - 1) * y), axis=-1).clamp(min=1e-9)
+    E_px_v = torch.mean(torch.log(pv_z))
+    Dkl = torch.mean(pz.log_prob(z) - q.log_prob(z))
+    l = E_px_v + Dkl
+    return -l
+
+
+
+
   
 
 
